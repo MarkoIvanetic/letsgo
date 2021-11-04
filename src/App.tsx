bfe586864c2ab501e370413aa6abd7a3585f6482
Marko Ivanetić <marko.ivanetic@burza.hr>
2021-11-04 20:44:53 +0100

    eslint done, for real

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { CssBaseline } from '@mui/material'
 import React from 'react'
 import Layout from './components/layout'
 
-const App: React.FC = (props) => {
-	console.log('I rendered')
+const App: React.FC = () => {
+    console.log('I rendered')
 
-	return <Layout>Webpack is cool!</Layout>
+    return (
+        <>
+            <CssBaseline />
+            <Layout>Webpack is cool!</Layout>
+        </>
+    )
 }
 
 export default App
