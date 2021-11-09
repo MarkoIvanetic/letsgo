d2610ba126297046cd97ac347b1b287ca04814b9
Marko Ivanetić <marko.ivanetic@burza.hr>
2021-11-09 14:02:22 +0100

    update eslint

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,8 @@
+import { Container } from '@mui/material'
 import React from 'react'
 
-const Layout: React.FC = ({children}) => {
-	return <div>{children}</div>
+const Layout: React.FC = ({ children }) => {
+    return <Container maxWidth="sm">{children}</Container>
 }
 
 export default Layout
