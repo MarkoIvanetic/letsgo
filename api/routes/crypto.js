284bee6b9620c18741b4e9275b3be4ff35407748
Marko Ivanetić <marko.ivanetic@burza.hr>
2021-11-11 13:03:03 +0100

    api

--- api/routes/crypto.js
+++ api/routes/crypto.js
@@ -2,17 +2,17 @@ var express = require('express')
 var fetch = require('node-fetch')
 var router = express.Router()
 
-const getRequestOptions = (apiKey = '7afe6b38-0b9c-49b1-82bc-c4da71e62640') => {
+const getRequestOptions = () => {
     return {
         method: 'GET',
         // uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
         qs: {
             start: '1',
-            limit: '5000',
+            limit: '50',
             convert: 'USD'
         },
         headers: {
-            'X-CMC_PRO_API_KEY': apiKey
+            'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY
         },
         json: true,
         gzip: true
@@ -24,11 +24,11 @@ router.get('/', async function (req, res) {
         route: { path }
     } = req
 
-    const apiKey = req.get('X-CMC_PRO_API_KEY')
     // const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency' + path, requestOptions)
-    const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', getRequestOptions(apiKey))
-
-    res.send(response)
+    const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', getRequestOptions())
+    const data = await response.json()
+    console.log(data)
+    res.send(data)
 })
 
 module.exports = router
